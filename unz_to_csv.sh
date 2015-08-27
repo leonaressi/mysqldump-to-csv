a0cef21ace7952fbc2c407db4fd0a80e232e1d18
root <root@instance-1.c.leo-naressi.internal>
2015-08-27 19:02:53 +0000

    fix stdouts redirects

--- unz_to_csv.sh
+++ unz_to_csv.sh
@@ -1,9 +1,5 @@
 for z in *.gz; do
 	echo adding $z... 
-	zcat $z | python /home/leonaressi//mysqldump-to-csv/mysqldump_to_csv.py >> $z.csv &
+	zcat $z | python /home/leonaressi//mysqldump-to-csv/mysqldump_to_csv.py > $z.csv &
 done
 
-for z in *.7z; do
-	echo adding $z... 
-        7zcat  $z | python /home/leonaressi/mysqldump-to-csv/mysqldump_to_csv.py >> $z.csv &
-done
